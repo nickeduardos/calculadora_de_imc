4288a951bdf4f9e11bb9b3f952be8bd066cf746c
nickeduardos <nickeduardogk@gmail.com>
2026-09-04 08:30:59 -0300

    Adicionado as solicitações ao usuário

--- CalcIMC.java
+++ CalcIMC.java
@@ -1,3 +1,12 @@
 void main () {
+    IO.println("==========CALCULO DE IMC==========");
+    IO.println();
+    String nome = IO.readln("Digite o seu Nome: ");
+    char genero = IO.readln("Digite o seu Genero [M] para Masculino, [F] para Feminino e [N] para não desejo informar: ").charAt(0);
+    String peso = IO.readln("Digite o seu peso: ");
+    String altura = IO.readln("Digite a sua altura: ");
+    double pesoDouble = Double.valueOf(peso);
+    double alturaDouble = Double.valueOf(altura);
+    
 
 }
